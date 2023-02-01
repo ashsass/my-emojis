31ef058fe5d845899616be51355709e930b5a4b3
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-02-01 11:30:22 -0500

    Added more functionality to the input and button to push an emoji to the array and display it on the page

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiCnt = document.getElementById('emojiContainer')
 const pushBtn = document.getElementById("push-btn")
-const emojiInput = document.getElementById("emoji-input")
 
-for (let i=0; i < myEmojis.length; i++) {
-    const newSpan = document.createElement("span")
-    newSpan.textContent += myEmojis[i];
-    emojiCnt.append(newSpan)
+render(myEmojis)
+
+function render(emojis){
+
+    for (let i=0; i < emojis.length; i++) {
+        const newSpan = document.createElement("span")
+        newSpan.textContent = emojis[i];
+        emojiCnt.append(newSpan)
+    }
+    
 }
 
-pushBtn.addEventListener('click', function(){
-    console.log(emojiInput.value)
+pushBtn.addEventListener('click', function(){ 
+    const emojiInput = document.getElementById("emoji-input")
+    if(emojiInput.value){
+        myEmojis.push(emojiInput.value) //Add the new emoji
+        emojiInput.value = ''; //Clear the input
+        emojiCnt.innerHTML = ""; //Clears the container to start fresh with new emoji
+        render(myEmojis)
+    }
 })
 
 
