8b42f8d7a93aa85c4acb64a953057661fd73735e
Ashlyn Sassaman <93344091+ashsass@users.noreply.github.com>
2023-02-01 11:05:22 -0500

    Added a text input field and a button to add emojis

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiCnt = document.getElementById('emojiContainer')
+const pushBtn = document.getElementById("push-btn")
+const emojiInput = document.getElementById("emoji-input")
 
 for (let i=0; i < myEmojis.length; i++) {
-    console.log(myEmojis[i]);
+    const newSpan = document.createElement("span")
+    newSpan.textContent += myEmojis[i];
+    emojiCnt.append(newSpan)
 }
+
+pushBtn.addEventListener('click', function(){
+    console.log(emojiInput.value)
+})
 
 
